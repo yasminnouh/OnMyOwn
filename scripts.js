9ce9a09c44c5479b76ab08456e9a0e637e3e07fe
Yasmin Nouh <yasminnouh@dyn-160-39-171-109.dyn.columbia.edu>
2015-03-24 17:54:00 -0400

    Changed myItem.push so that it's pushing a string.

--- scripts.js
+++ scripts.js
@@ -5,13 +5,13 @@
 //credit Google with code sample 
  
  
+ 
  google.load('visualization', '1', {packages: ['corechart']});
     google.setOnLoadCallback(drawChart);
 
     function drawChart() {
     	
     	var obesityDataArray = [];
-    	var vInterval = [];
     	
     	//create an array to hold "tick" dates
     	
@@ -23,15 +23,24 @@
     		//as well as the "myItem" array
     		myItem.push(new Date(obesityData[i].year, 0, 1));
     		myItem.push(obesityData[i].percent);
+    		
+    		if(obesityData[i].percent == 19.9){
+    			myItem.push("My annotation");
+    		}else{
+    			myItem.push("");
+    		}
     		obesityDataArray.push(myItem);
+    		
     	}
 		
 		//console.log(obesityDataArray);	
       var data = new google.visualization.DataTable();
       data.addColumn('date', 'Year');
       data.addColumn('number', 'NYC Obesity Rate');
-
+	  data.addColumn({type:'string', role:'annotation'});
       data.addRows(obesityDataArray);
+
+var verticalAxis = { ticks: [5,10,15,20] }
 
       var options = {
         width: 1000,
@@ -41,7 +50,8 @@
         },
         vAxis: {
           title: 'Percent of Population'
-        }
+        },
+        verticalAxis: {}
       };
 
       var chart = new google.visualization.LineChart(
